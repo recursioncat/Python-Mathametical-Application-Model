5ae87dc48ced27c7e17206c837ce6e53c6c04933
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2023-02-18 23:05:15 +0530

    Update Matrix.py

--- Python Mathametical Application Model/Matrix.py
+++ Python Mathametical Application Model/Matrix.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 from copy import deepcopy
+import numpy as np
 
 # Load Matrix
 
@@ -13,13 +14,17 @@ def load_matrix(x=None):                                                      #I
         matrix = [[int(l) for l in row] for row in matrix]                    #converts each value into an int                
         print("\nData Loaded Successfully")
         return pd.DataFrame(matrix)
+    
     if(type(x)==list):                                                        #if Function passed as a list
         matrix = x
         return pd.DataFrame(matrix)
-    if(type(x)=numpy.ndarray):
+    
+    if(type(x)==numpy.ndarray):
         return pd.DataFrame(x)
+    
     else:
         print("Input Format Not Supported")
+        return None
 
 #Find Sum Of Matrix
 
@@ -36,9 +41,9 @@ def mat_diff(a, b):
 #Determinant
 
 def smaller_matrix(a,i):
-    l = deepcopy(a)
+    l = deepcopy(a)                                                  #Copy The Matrix
     l.drop(l.columns[i], axis=1, inplace=True)
-    l.columns=[num for num in range(0,l.shape[1])]
+    l.columns=[num for num in range(0,l.shape[1])]                   #Drop And Rename The Columns and Rows
     l.drop([0], inplace=True)
     l.index=[num for num in range(0,l.shape[0])]
     return l
@@ -51,16 +56,28 @@ def det(a):
         return None
     
     elif(num_rows[0]==2):
-        product=(a.loc[0,0]*a.loc[1,1])-(a.loc[0,1]*a.loc[1,0])
+        product=(a.loc[0,0]*a.loc[1,1])-(a.loc[0,1]*a.loc[1,0])       #Determinant Of a 2x2 Square Matrix
         return product
     
     else:
         f=0
         for i in range(0, num_rows[0]):
-            m=a.loc[0,i]*(-1)**i*det(smaller_matrix(a,i))
+            m=a.loc[0,i]*(-1)**i*det(smaller_matrix(a,i))             #Pos * (-1)^ position * determinant of smaller Matrix
             f+=m
         return f 
 
 
-s= load_matrix()
-print(det(s))
+# Multiplication
+def mul(mat1,mat2):
+    if(mat1.shape[0] != mat2.shape[1]):
+        print("Multiplication Of Matrices Not Possible")
+        return None
+    else:
+        prod = pd.DataFrame(index=range(mat1.shape[0]), columns=range(mat2.shape[1]))   #Create An Empty Dataframe
+        for i in range(prod.shape[0]):
+            for j in range(prod.shape[1]):
+                right = mat1.iloc[i, :].values                                          #List of Rows of the First Matrix
+                down = mat2.iloc[:, j].values                                           #List of Columns of The Second Matrix (Waterfall Method)
+                res = np.dot(right, down)
+                prod.iloc[i, j] = res
+        return prod
