a4d7873de643c805fe7afa7e224aa0c342fc9f2c
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2023-08-15 00:49:30 +0530

    Update main.py
    Added Fraction Processing and Multiple terms handling for same coefficients

--- Python Mathametical Application Model/Quadratic Equations/main.py
+++ Python Mathametical Application Model/Quadratic Equations/main.py
@@ -1,45 +1,10 @@
-import re
 import math
+import re
 
-def arrange_eq(var):   
-    var=var.replace(" ", "")
-    sttr=""
-    for items in var:
-        if(items=="+"):
-            sttr+=" +"
-        elif(items=="-"):
-            sttr+=" -"
-        else:
-            sttr+=items
-    sttr = sttr[:-2]
-    if(sttr[0]!="-"):
-        sttr="+"+sttr
-
-    lst=["", "", ""]
-    split=sttr.split()
-
-    for item in split:
-        if item.endswith("x**2"):
-            lst[0]=item
-        elif item.endswith("x"):
-            lst[1]=item
-        else:
-            lst[2]=item
-
-    return (lst_to_str(lst)+"=0")
-
-def add_zeros(var):
-    split=re.split("=",var)
-    split2=re.split("[+|-]", split[0])
-    count=0
-    for item in split2:
-        if(item.endswith("x")):
-            count=1
-    if(count==0):
-        var="0x+"+var
-        return var
-    else:
-        return var
+def process_division(eq):
+    eq=eq.replace("(", "").replace(")", "")
+    split2=re.split("/", eq)
+    return float(int(split2[0])/int(split2[1]))
 
 def change_signs(equa):
     split=tokens=re.split(r"=", equa)
@@ -54,48 +19,56 @@ def change_signs(equa):
         else:
             emp+="+"
 
-    return (split[0]+emp+"=0")
+    return (split[0]+emp)
 
-def eq_split(eq): #takes what it needs
-    
-    split=re.split(r"=", eq)
-    if(split[1]!="0"):
-        eq=change_signs(eq)
-        
-    print(eq)
-    eq=add_zeros(eq)
-    print(eq)
-    eq=arrange_eq(eq)
-    print(eq)
-    
-    tokens=re.split(r"x\*\*2|x|=", eq)
-    while("" in tokens):
-        tokens.remove("")
-    coeff={}
-    
-    try:
-        coeff["a"] = eval(tokens[0])
-    except:
-        coeff["a"] = 1
-        
-    try:
-        coeff["b"] = eval(tokens[1])
-    except:
-        coeff["b"] = 1
-        
-    try:
-        coeff["c"] = eval(tokens[2])
-    except:
-        coeff["c"] = 0
-    
+def process(var):
+    var = var.replace(" ", "")  # remove =0 and then remove all spaces
+    var = var.replace("X","x")
+    # Add Spaces between each term
+    sttr = ""
+    for items in var:
+        if items == "+":
+            sttr += " +"
+        elif items == "-":
+            sttr += " -"
+        else:
+            sttr += items
+    if sttr[0] != "-":
+        sttr = "+" + sttr
+    split = re.split(" ", sttr)
+    for i in range(len((split))):
+        if (split[i][1:] == "x^2") or (split[i][1:] == "x**2") or (split[i][1:] == "x") or (split[i][1:] == "x2"):
+                split[i] = split[i][0] + "1" + split[i][1:]
+    print(split)
+            
+    # Create an ultimate dictionary
+    coeff = {"b": 0, "a": 0, "c": 0}
+    for item in split:
+        if (item.endswith("x^2") or item.endswith("x**2") or item.endswith("x2")):
+            if("/" in item):
+                var=process_division(re.split("x\^2|x\*\*2|x2", item)[0])
+                coeff["a"] += var
+            else:
+                coeff["a"] += float(re.split("x\^2|x\*\*2|x2", item)[0])
+        elif item.endswith("x"):
+            if("/" in item):
+                var=process_division(re.split("x", item)[0])
+                coeff["b"] += var
+            else:
+                coeff["b"] += float(re.split("x", item)[0])
+        else:
+            if("/" in item):
+                var=process_division(item)
+                coeff["c"] += var
+            else:
+                coeff["c"] += float(item)
+
     return coeff
-
-
+  
 def quad(eq): #solves
-    coeff=eq_split(eq)
-    for key in coeff:
-        coeff[key] = int(coeff[key])
-    
+    eq=change_signs(eq)
+    coeff=process(eq)
+    print(f"coeff:{coeff}")    
     delta = coeff["b"]**2 - 4*coeff["a"]*coeff["c"]
     if delta < 0:
         print("There are no real roots.")
@@ -109,5 +82,3 @@ def quad(eq): #solves
         sol2 = (-coeff["b"] - math.sqrt(delta)) / (2*coeff["a"])
         print(f"The two real roots are {sol1} and {sol2}")
         return [sol1,sol2]
-    
-quad("4x**2=-5x+6")
