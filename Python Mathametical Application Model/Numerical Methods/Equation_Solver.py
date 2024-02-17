12dd122011ee5d829225ba9a2fb4ef27b665b75f
Rishit Chakraborty <125457882+recursioncat@users.noreply.github.com>
2024-02-17 11:56:48 +0530

    Added a Intelligent Equation Parser

--- Python Mathametical Application Model/Numerical Methods/Equation_Solver.py
+++ Python Mathametical Application Model/Numerical Methods/Equation_Solver.py
@@ -1,30 +1,20 @@
-from temp import *
+from parser_evaluater import *
 import random
 import sympy as sp
 
-equation = input("Enter Equation: ")
-de = str(sp.diff(equation,  "x")).replace("*", "").replace(" ","")
-if "x+" not in de or "x-" not in de:
-    de = de.replace("x", "x^")
-if  "+x" not in de or "-x" not in de:
-    de = de.replace("x", "*x")
 
-max_steps = 1000
-some_number = random.randint(-10, 10)
-
-
-steps = 0
-value = 1
-while steps<=max_steps:
-    value = evaluate_equation(equation, some_number)
-    dv = evaluate_equation(de, some_number)
-    temp = some_number - (value/dv)
-    some_number = temp
-    steps += 1
-    if value == 0:
-        break
-
-print(some_number)
-
-
-# fix parsing issues
+def find_root(equation, max_steps = 1000, root = random.randint(-10, 10)): 
+    equation = parse(equation)
+    diff = sp.diff(equation) 
+    steps = 0
+    value = 1
+    while steps<=max_steps:
+        value = evaluate_equation(equation, root)
+        dv = evaluate_equation(diff, root)
+        temp = root - (value/dv)
+        #print(f"{root} - {value} / {dv}")
+        root = temp
+        steps += 1
+        if value == 0:
+            break
+    return root
